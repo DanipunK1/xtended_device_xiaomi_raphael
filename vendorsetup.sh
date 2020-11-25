a4b0f0ace743039b69b984cf79bfd2f2367583f4
DanipunK1 <pegate.eltrueco@gmail.com>
2020-11-25 15:17:14 +0100

    new hals from streak

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,7 @@
 export SKIP_ABI_CHECKS=true
-export SELINUX_IGNORE_NEVERALLAOWS=true
 rm -rf hardware/qcom-caf/sm8150/display
 rm -rf hardware/qcom-caf/sm8150/media
 rm -rf hardware/qcom-caf/sm8150/audio
-git clone https://github.com/HSgod/hardware_qcom-caf_sm8150_display.git -b eleven hardware/qcom-caf/sm8150/display
-git clone https://github.com/HSgod/hardware_qcom-caf_sm8150_media.git -b eleven hardware/qcom-caf/sm8150/media
-git clone https://github.com/HSgod/hardware_qcom-caf_sm8150_audio.git -b eleven hardware/qcom-caf/sm8150/audio
+git clone https://github.com/ProjectStreak/platform_hardware_qcom-caf_sm8150_display.git -b eleven hardware/qcom-caf/sm8150/display
+git clone https://github.com/ProjectStreak/platform_hardware_qcom-caf_sm8150_media.git -b eleven hardware/qcom-caf/sm8150/media
+git clone https://github.com/ProjectStreak/platform_hardware_qcom-caf_sm8150_audio.git -b eleven hardware/qcom-caf/sm8150/audio
